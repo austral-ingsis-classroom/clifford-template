787c41fcc38479012d910455e5a6338cdcbff5ec
carla <cboggio@mail.austral.edu.ar>
2024-06-24 19:32:23 -0300

    pass almost all the tests

--- src/main/java/edu/austral/ingsis/clifford/Commands/CDCommand.java
+++ src/main/java/edu/austral/ingsis/clifford/Commands/CDCommand.java
@@ -18,28 +18,41 @@ public class CDCommand implements Command {
     @Override
     public String execute() {
         Directory currentDirectory = fileSystem.getCurrentDirectory();
+
         if (path.equals(".")) {
             return "moved to directory '" + currentDirectory.getName() + "'";
         }
+
         if (path.equals("..")) {
-            if (currentDirectory.getParent() != null) {
-                fileSystem.setCurrentDirectory(currentDirectory.getParent());
+            Directory parent = currentDirectory.getParent();
+            if (parent != null) {
+                fileSystem.setCurrentDirectory(parent);
             }
+            return "moved to directory '" + (parent != null ? parent.getName() : "/") + "'";
+        }
+
+        if (path.equals("/")) {
+            fileSystem.setCurrentDirectory(fileSystem.getRoot());
             return "moved to directory '/'";
         }
+
         if (path.startsWith("/")) {
             currentDirectory = fileSystem.getRoot();
-            path = path.substring(1);
         }
+
         String[] parts = path.split("/");
         for (String part : parts) {
+            if (part.isEmpty()) {
+                continue;
+            }
             Optional<FileSystemItem> next = currentDirectory.findItem(part);
-            if (next.isPresent() && next.get().isDirectory()) {
+            if (next.isPresent() && next.get() instanceof Directory) {
                 currentDirectory = (Directory) next.get();
             } else {
                 return "'" + part + "' directory does not exist";
             }
         }
+
         fileSystem.setCurrentDirectory(currentDirectory);
         return "moved to directory '" + currentDirectory.getName() + "'";
     }
